d6822399ed623db89be6fc52646ec5805a7b0b7c
borjauz <91206769+borjauz@users.noreply.github.com>
2022-03-10 17:06:46 +0100

    Añado suma de 3 naturales

---
+++ Suma3Naturales.java
@@ -0,0 +1,14 @@
+package apoyoTeoria;
+
+public class Suma3Naturales {
+	//suma de los 3 primeros numero naturales
+	public static void main(String[] args) {
+			int suma=0;
+			int numero=1;
+			while (numero<=3){
+				suma+=numero;
+				numero++;
+				}
+			System.out.println(suma);
+	}
+}
